a25474810c0a23d7925ad93441f3716a06743965
Ruben E Acosta <eldragondera1@gmail.com>
2020-06-28 13:05:52 -0500

    configurando apollo

---
+++ client/pages/_app.js
@@ -0,0 +1,13 @@
+// Archivo principal
+import { ApolloProvider } from '@apollo/client';
+import Client from '../config/apollo';
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ApolloProvider client={Client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
+
+export default MyApp;
